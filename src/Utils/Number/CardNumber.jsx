24aef8755423591569cf9a00f8b0a23b5bc44fe5
Neelesh Joshi <neeleshjoshi001@gmail.com>
2023-08-07 08:19:59 +0530

    improved card numbering

--- src/Utils/Number/CardNumber.jsx
+++ src/Utils/Number/CardNumber.jsx
@@ -1,5 +1,5 @@
 export default function CardNumber(props){
     return(
-        <p className="absolute flex justify-center items-center font-bold rounded-tr-xl text-2xl bg-black text-yellow-300 w-16 h-16 top-0 right-0" >{props.numberId + 1}</p>
+        <p className="absolute flex justify-center items-center font-bold rounded-tr-xl text-2xl bg-black text-yellow-300 w-6 h-6 top-0 right-0" >{props.numberId + 1}</p>
     )
 }
